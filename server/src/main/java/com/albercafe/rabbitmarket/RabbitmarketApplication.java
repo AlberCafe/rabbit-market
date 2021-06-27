8414d17d1c78fe745e5e0e869f79e2d2db2bc315
sdy <vel1024@khu.ac.kr>
2021-06-28 00:29:51 +0900

    config initial server

---
+++ server/src/main/java/com/albercafe/rabbitmarket/RabbitmarketApplication.java
@@ -0,0 +1,13 @@
+package com.albercafe.rabbitmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmarketApplication.class, args);
+	}
+
+}
