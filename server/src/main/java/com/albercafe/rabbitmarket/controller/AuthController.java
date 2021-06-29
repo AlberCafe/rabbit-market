80508b1a087cc7e1fe82e9c0852a000425387ba1
sdy <vel1024@khu.ac.kr>
2021-06-29 19:46:39 +0900

    create AuthController

---
+++ server/src/main/java/com/albercafe/rabbitmarket/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.albercafe.rabbitmarket.controller;
+
+import com.albercafe.rabbitmarket.dto.RegisterRequest;
+import com.albercafe.rabbitmarket.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("회원가입 완료", HttpStatus.OK);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("계정 활성화가 성공적으로 되었습니다.", HttpStatus.OK);
+    }
+}
