8f9d73c3b07ffd8013c0f2f3d9af0a51535da07c
sdy <vel1024@khu.ac.kr>
2021-07-01 19:09:40 +0900

    create UserDetailsServiceImpl

---
+++ server/src/main/java/com/albercafe/rabbitmarket/service/UserDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package com.albercafe.rabbitmarket.service;
+
+import com.albercafe.rabbitmarket.entity.User;
+import com.albercafe.rabbitmarket.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        User user = userOptional
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없음 : " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                getAuthorities("USER")
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+}
