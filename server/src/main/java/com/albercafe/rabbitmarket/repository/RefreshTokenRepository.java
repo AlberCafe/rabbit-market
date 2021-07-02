90b50a112094b935a55badec91e09e1efb792b9b
sdy <vel1024@khu.ac.kr>
2021-07-02 18:06:04 +0900

    create RefreshTokenRepository

---
+++ server/src/main/java/com/albercafe/rabbitmarket/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.albercafe.rabbitmarket.repository;
+
+import com.albercafe.rabbitmarket.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String token);
+}
