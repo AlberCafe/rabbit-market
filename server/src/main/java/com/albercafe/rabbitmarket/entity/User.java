95ecc523164099e7afc99217a01288e740cecd80
sdy <vel1024@khu.ac.kr>
2021-07-09 22:40:54 +0900

    update User Entity

--- server/src/main/java/com/albercafe/rabbitmarket/entity/User.java
+++ server/src/main/java/com/albercafe/rabbitmarket/entity/User.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -20,7 +18,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    private String username;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_profile_id", referencedColumnName = "id")
+    private UserProfile userProfile;
 
     @NotBlank(message = "password 는 빈 값이 들어갈 수 없습니다.")
     private String password;
@@ -29,17 +29,9 @@ public class User {
     @NotEmpty(message = "email 은 빈 값이 들어갈 수 없습니다.")
     private String email;
 
-    @Pattern(
-            regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})",
-            message = "010-1234-5678 로 입력하거나 010 1234 5678 로 입력해주세요"
-    )
-    private String phoneNumber;
-
     // 이메일 혹은 핸드폰을 통해 인증된 유저인지 파악
     private boolean enabled;
 
-    // TODO: 별도의 엔티티로 차후에 분리될 수 있음
-    private String address;
-
-    // TODO: 프로필 사진은 별도의 엔티티 선언
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Product> productList;
 }
