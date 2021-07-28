4675d8a575fb6c02e5a7b742652818a89f50ddf8
sdy <vel1024@khu.ac.kr>
2021-07-28 23:14:59 +0900

    refactoring Product Entity

--- server/src/main/java/com/albercafe/rabbitmarket/entity/Product.java
+++ server/src/main/java/com/albercafe/rabbitmarket/entity/Product.java
@@ -1,13 +1,16 @@
 package com.albercafe.rabbitmarket.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
+import javax.validation.constraints.Pattern;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Data
@@ -26,19 +29,28 @@ public class Product {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @JsonIgnore
     private Category category;
 
-    @NotNull(message = "이름은 필수 값입니다.")
-    private String name;
+    @NotNull(message = "중고거래 제목은 필수 입니다.")
+    private String title;
 
     @CreatedDate
-    private LocalDateTime createdDate;
+    private OffsetDateTime createdDate;
 
     @Lob
     @Column(name = "content", length = 1024)
+    @NotBlank(message = "내용은 최소 한글자 이상이어야 합니다.")
     private String content;
+
+    @Pattern(
+            regexp = "^(0|[1-9]\\d*)$",
+            message = "매물 값은 양의 정수만 가능합니다."
+    )
+    private String price;
 }
