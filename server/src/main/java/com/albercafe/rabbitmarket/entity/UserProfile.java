921244baf779e72b5f1d5b38dea0fdf8a1dea684
sdy <vel1024@khu.ac.kr>
2021-07-28 23:12:55 +0900

    add @JsonIgnore

--- server/src/main/java/com/albercafe/rabbitmarket/entity/UserProfile.java
+++ server/src/main/java/com/albercafe/rabbitmarket/entity/UserProfile.java
@@ -1,5 +1,6 @@
 package com.albercafe.rabbitmarket.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -36,5 +37,6 @@ public class UserProfile {
     private Long ratings;
 
     @OneToOne(mappedBy = "userProfile")
+    @JsonIgnore
     private User user;
 }
