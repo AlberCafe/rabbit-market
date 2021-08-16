b7e26578e9f16ff2a4aada3d527652cf6c41b423
sdy <vel1024@khu.ac.kr>
2021-08-16 18:40:30 +0900

    add static file link

--- server/src/main/java/com/albercafe/rabbitmarket/util/Constants.java
+++ server/src/main/java/com/albercafe/rabbitmarket/util/Constants.java
@@ -5,4 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
     public static final String ACTIVATION_EMAIL = "http://ec2-3-35-167-70.ap-northeast-2.compute.amazonaws.com:8080/api/auth/accountVerification";
+    public static final String STATIC_FILE_LINK = "https://rabbit-market-static-files.s3.ap-northeast-2.amazonaws.com";
 }
