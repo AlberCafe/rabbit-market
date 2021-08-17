58b5af4527293102b1ab99d240a1335d146a85a2
sdy <vel1024@khu.ac.kr>
2021-08-17 14:46:46 +0900

    refactoring deleteRefreshToken logic

--- server/src/main/java/com/albercafe/rabbitmarket/service/RefreshTokenService.java
+++ server/src/main/java/com/albercafe/rabbitmarket/service/RefreshTokenService.java
@@ -2,10 +2,12 @@ package com.albercafe.rabbitmarket.service;
 
 import com.albercafe.rabbitmarket.dto.CustomResponse;
 import com.albercafe.rabbitmarket.entity.RefreshToken;
+import com.albercafe.rabbitmarket.entity.User;
 import com.albercafe.rabbitmarket.exception.InvalidRefreshTokenException;
 import com.albercafe.rabbitmarket.exception.TokenNotFoundException;
 import com.albercafe.rabbitmarket.repository.RefreshTokenRepository;
-import lombok.AllArgsConstructor;
+import com.albercafe.rabbitmarket.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +17,12 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Service
-@AllArgsConstructor
 @Transactional
+@RequiredArgsConstructor
 public class RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
 
     RefreshToken generateRefreshToken() {
         RefreshToken refreshToken = new RefreshToken();
@@ -47,9 +50,16 @@ public class RefreshTokenService {
     public ResponseEntity<CustomResponse> deleteRefreshToken(String token) {
         CustomResponse responseBody = new CustomResponse();
 
+        RefreshToken refreshToken = refreshTokenRepository.findByToken(token).orElseThrow(() -> new TokenNotFoundException(token));
+        User user = refreshToken.getUser();
+
+        user.setRefreshToken(null);
+
+        userRepository.save(user);
+
         refreshTokenRepository.deleteByToken(token);
 
-        responseBody.setData(token + " is removed !, you need to login again ! ");
+        responseBody.setData("refresh token : " + token + " is removed !, you need to login again ! ");
         responseBody.setError(null);
 
         return ResponseEntity.status(200).body(responseBody);
