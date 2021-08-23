28b4e31d01cbeaeb87726fd88d32201eafd7beee
sdy <vel1024@khu.ac.kr>
2021-08-23 21:31:49 +0900

    add some string constants

--- server/src/main/java/com/albercafe/rabbitmarket/util/Constants.java
+++ server/src/main/java/com/albercafe/rabbitmarket/util/Constants.java
@@ -6,4 +6,7 @@ import lombok.experimental.UtilityClass;
 public class Constants {
     public static final String ACTIVATION_EMAIL = "http://ec2-3-35-167-70.ap-northeast-2.compute.amazonaws.com:8080/api/auth/accountVerification";
     public static final String STATIC_FILE_LINK = "https://rabbit-market-static-files.s3.ap-northeast-2.amazonaws.com";
+    public static final String MISSING_USER_INFO_URI_ERROR_CODE = "missing_user_info_uri";
+    public static final String MISSING_USER_NAME_ATTRIBUTE_ERROR_CODE = "missing_user_name_attribute";
+    public static final String INVALID_USER_INFO_RESPONSE_ERROR_CODE = "invalid_user_info_response";
 }
