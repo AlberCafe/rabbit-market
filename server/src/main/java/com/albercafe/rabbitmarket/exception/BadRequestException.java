999e735c60c683a72dc8c284199a2b48d3afeafb
sdy <vel1024@khu.ac.kr>
2021-08-27 20:22:47 +0900

    create custom exception class

---
+++ server/src/main/java/com/albercafe/rabbitmarket/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.albercafe.rabbitmarket.exception;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
