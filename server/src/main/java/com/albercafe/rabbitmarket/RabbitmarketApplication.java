06ca902c85638de93080372c4bf0a84b01dd7258
sdy <vel1024@khu.ac.kr>
2021-08-27 20:40:42 +0900

    add @EnableConfigurationProperties

--- server/src/main/java/com/albercafe/rabbitmarket/RabbitmarketApplication.java
+++ server/src/main/java/com/albercafe/rabbitmarket/RabbitmarketApplication.java
@@ -1,11 +1,14 @@
 package com.albercafe.rabbitmarket;
 
+import com.albercafe.rabbitmarket.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableConfigurationProperties(AppProperties.class)
 public class RabbitmarketApplication {
 
 	public static void main(String[] args) {
