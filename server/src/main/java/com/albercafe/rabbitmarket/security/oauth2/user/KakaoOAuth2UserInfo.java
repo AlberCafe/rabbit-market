333277422f4e1c71c834cb09517ac57bef00d5bb
sdy <vel1024@khu.ac.kr>
2021-08-27 20:23:58 +0900

    create OAuth2 Provider information class

---
+++ server/src/main/java/com/albercafe/rabbitmarket/security/oauth2/user/KakaoOAuth2UserInfo.java
@@ -0,0 +1,31 @@
+package com.albercafe.rabbitmarket.security.oauth2.user;
+
+import java.util.Map;
+
+public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
+    public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("profile_nickname");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("account_email");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) attributes.get("profile_image");
+    }
+
+
+}
