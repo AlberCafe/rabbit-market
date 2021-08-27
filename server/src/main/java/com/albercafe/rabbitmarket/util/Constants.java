3e3adfac76354ec8d6b67fc86795a180ee5fc5a7
sdy <vel1024@khu.ac.kr>
2021-08-27 20:41:11 +0900

    update constants

--- server/src/main/java/com/albercafe/rabbitmarket/util/Constants.java
+++ server/src/main/java/com/albercafe/rabbitmarket/util/Constants.java
@@ -6,7 +6,8 @@ import lombok.experimental.UtilityClass;
 public class Constants {
     public static final String ACTIVATION_EMAIL = "http://ec2-3-35-167-70.ap-northeast-2.compute.amazonaws.com:8080/api/auth/accountVerification";
     public static final String STATIC_FILE_LINK = "https://rabbit-market-static-files.s3.ap-northeast-2.amazonaws.com";
-    public static final String MISSING_USER_INFO_URI_ERROR_CODE = "missing_user_info_uri";
-    public static final String MISSING_USER_NAME_ATTRIBUTE_ERROR_CODE = "missing_user_name_attribute";
-    public static final String INVALID_USER_INFO_RESPONSE_ERROR_CODE = "invalid_user_info_response";
+    public static final String AUTHORIZATION_REQUEST_BASE_URL = "oauth2/authorization";
+    public static final String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
 }
