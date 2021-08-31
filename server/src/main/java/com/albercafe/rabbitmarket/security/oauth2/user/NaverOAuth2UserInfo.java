ac8fd7c9e453bb12072f6bab08e0e077eee22538
sdy <vel1024@khu.ac.kr>
2021-08-31 17:36:43 +0900

    update naver response value

--- server/src/main/java/com/albercafe/rabbitmarket/security/oauth2/user/NaverOAuth2UserInfo.java
+++ server/src/main/java/com/albercafe/rabbitmarket/security/oauth2/user/NaverOAuth2UserInfo.java
@@ -9,21 +9,21 @@ public class NaverOAuth2UserInfo extends OAuth2UserInfo {
 
     @Override
     public String getId() {
-        return (String) attributes.get("id");
+        return (String) attributes.get("response/id");
     }
 
     @Override
     public String getName() {
-        return (String) attributes.get("name");
+        return (String) attributes.get("response/name");
     }
 
     @Override
     public String getEmail() {
-        return (String) attributes.get("email");
+        return (String) attributes.get("response/email");
     }
 
     @Override
     public String getImageUrl() {
-        return (String) attributes.get("profile_image");
+        return (String) attributes.get("response/profile_image");
     }
 }
