2fa8fedb4be980710c57394cfdc2610fbf149154
sdy <vel1024@khu.ac.kr>
2021-09-03 01:51:14 +0900

    add update method

--- server/src/main/java/com/albercafe/rabbitmarket/entity/User.java
+++ server/src/main/java/com/albercafe/rabbitmarket/entity/User.java
@@ -24,7 +24,6 @@ public class User {
     @JoinColumn(name = "user_profile_id")
     private UserProfile userProfile;
 
-    @NotNull(message = "password 는 빈 값이 들어갈 수 없습니다.")
     private String password;
 
     @Email
@@ -52,4 +51,14 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(name = "ROLE_TYPE", columnDefinition = "varchar(20) default 'GUEST'")
     private RoleType roleType;
+
+    public User update(String username, String picture, AuthProvider authProvider) {
+        UserProfile userProfile = this.getUserProfile();
+        userProfile.setUsername(username);
+        userProfile.setProfilePhoto(picture);
+        this.setUserProfile(userProfile);
+        this.setProvider(authProvider);
+
+        return this;
+    }
 }
