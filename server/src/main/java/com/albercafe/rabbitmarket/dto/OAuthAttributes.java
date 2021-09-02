939a44b7836595846068d3e5b3468b7c23d4f026
sdy <vel1024@khu.ac.kr>
2021-09-03 01:51:00 +0900

    add kakao, naver, google

--- server/src/main/java/com/albercafe/rabbitmarket/dto/OAuthAttributes.java
+++ server/src/main/java/com/albercafe/rabbitmarket/dto/OAuthAttributes.java
@@ -20,9 +20,53 @@ public class OAuthAttributes {
     private final String email;
     private final String picture;
     private final AuthProvider authProvider;
+    private final Boolean enabled;
 
     // OAuth2User 에서 리턴하는 사용자 정보가 Map 이어서 하나씩 변환
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+
+        if ("naver".equals(registrationId)) {
+            return ofNaver(registrationId, userNameAttributeName, attributes);
+        }
+
+        if ("kakao".equals(registrationId)) {
+            return ofKakao(registrationId, userNameAttributeName, attributes);
+        }
+
+        return ofGoogle(registrationId, userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .username((String) response.get("nickname"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .authProvider(AuthProvider.valueOf(registrationId.toUpperCase()))
+                .enabled(true)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+
+        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .username((String) kakaoProfile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String) kakaoProfile.get("profile_image_url"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .authProvider(AuthProvider.valueOf(registrationId.toUpperCase()))
+                .enabled(true)
+                .build();
+    }
+
+    private static OAuthAttributes ofGoogle(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .username((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -30,6 +74,7 @@ public class OAuthAttributes {
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .authProvider(AuthProvider.valueOf(registrationId.toUpperCase()))
+                .enabled(true)
                 .build();
     }
 
@@ -43,6 +88,8 @@ public class OAuthAttributes {
         user.setEmail(email);
         user.setRoleType(RoleType.USER);
         user.setUserProfile(userProfile);
+        user.setProvider(authProvider);
+        user.setEnabled(enabled);
 
         return user;
     }
