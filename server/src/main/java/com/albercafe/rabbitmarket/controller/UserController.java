ed8d9c16fea4dcdea17af4bbb0f189b4c2957047
sdy <vel1024@khu.ac.kr>
2021-09-03 00:15:49 +0900

    remove unused method

--- server/src/main/java/com/albercafe/rabbitmarket/controller/UserController.java
+++ server/src/main/java/com/albercafe/rabbitmarket/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.albercafe.rabbitmarket.controller;
 
-import com.albercafe.rabbitmarket.annotation.CurrentUser;
 import com.albercafe.rabbitmarket.dto.CustomResponse;
 import com.albercafe.rabbitmarket.dto.UserProfileRequest;
-import com.albercafe.rabbitmarket.security.UserPrincipal;
 import com.albercafe.rabbitmarket.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -31,13 +28,5 @@ public class UserController {
     @PatchMapping("/{id}/profile")
     public ResponseEntity<CustomResponse> updateUserProfile(@PathVariable Long id, @Valid @RequestBody UserProfileRequest userProfileRequest) {
         return userService.updateUserProfile(id, userProfileRequest);
-    }
-
-    @ApiOperation(httpMethod = "GET", value = "Get Current Login user information", notes = "This method return current user's detail information")
-    @ApiResponse(code = 200, message = "OK", response = CustomResponse.class)
-    @GetMapping("/user/me")
-    @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<CustomResponse> getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
-        return userService.getCurrentUser(userPrincipal);
     }
 }
