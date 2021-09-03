c32100473e4a6e817e8ec75c376af4c738d7e5e7
sdy <vel1024@khu.ac.kr>
2021-09-03 15:44:14 +0900

    remove unnecessary classes

--- server/src/main/java/com/albercafe/rabbitmarket/exception/ProductNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.albercafe.rabbitmarket.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message) { super(message); }
-}
--- server/src/main/java/com/albercafe/rabbitmarket/exception/UserProfileNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.albercafe.rabbitmarket.exception;
-
-public class UserProfileNotFoundException extends RuntimeException{
-    public UserProfileNotFoundException(String message) { super(message); }
-}
